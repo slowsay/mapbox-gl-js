79deaa7a18071bf66b26813f2bfa69d1c63699fd
Lucas Wojciechowski <lucas@mapbox.com>
2016-10-21 14:29:03 -0700

    Extract script detection code into util module

--- js/util/script_detection.js
+++ js/util/script_detection.js
@@ -9,7 +9,6 @@ module.exports.allowsIdeographicBreaking = function(chars) {
     return true;
 };
 
-
 module.exports.charAllowsIdeographicBreaking = function(char) {
     // Return early for characters outside all ideographic ranges.
     if (char < 0x2E80) return false;
@@ -51,4 +50,26 @@ module.exports.charAllowsIdeographicBreaking = function(char) {
     if (char >= 0xFF00 && char <= 0xFFEF) return true;
 
     return false;
+};
+
+const isVerticalWritingModeRegExp = new RegExp([
+    '[\u1100-\u11FF]',
+    '[\uAC00-\uD7A3]',
+    '[\u3131-\u318E]',
+    '[\u4E00-\u9FCC]',
+    '[\u3400-\u4DB5]',
+    '[\u3000-\u303F]',
+    '[\uFF01-\uFFEE]',
+    '[\u3041-\u309F]',
+    '[\u30A0-\u30FF]',
+    '[\u31F0-\u31FF]',
+    '[\uA000-\uA4C6]',
+    '[\u1800-\u18AA]',
+    '\uD840[\uDC00-\uFFFF]',
+    '[\uD841-\uD872]',
+    '\uD873[\u0000-\uDEAF]'
+].join('|'));
+
+module.exports.allowsVerticalWritingMode = function(input) {
+    return input.search(isVerticalWritingModeRegExp) !== -1;
 };
